c5cbab82af279f8581963317bd642c575eb97c06
Marcella_Mac84 <macedomarcella84@gmail.com>
2021-04-22 16:09:53 -0300

    Add files via upload

---
+++ Dict_Listas.py
@@ -0,0 +1,44 @@
+#Programa de leitura de nome, sexo e idade de várias pessoas
+# armazena os dados de cada pessoa em um dicionário e todos os dicionários em uma lista
+
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'1º) Ao todo temos {len(galera)} pessoas cadastradas. ')
+media = soma / len(galera)
+print(f'2º) A média de idade é de {media:5.2f} anos')
+print('3º) As mulheres cadastradas foram: ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ', end='')
+print()
+print('4º) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('   ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
+
+
+
